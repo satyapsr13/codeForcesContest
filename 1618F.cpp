13327182d3a9005bdb1f7417771c1cc21d3f9269
satyaprakashsinghrathour <satyapsr13@gmail.com>
2021-12-15 17:06:11 -0500

    5

--- 1618F.cpp
+++ 1618F.cpp
@@ -3,31 +3,28 @@
 using namespace std;
 #define int long long int
 #define count_1 __builtin_popcountll
-long long power(long long a, long long b, long long md)
-{
-    return (!b ? 1 : (b & 1 ? a * power(a * a % md, b / 2, md) % md : power(a * a % md, b / 2, md) % md));
-}
+long long power(long long a, long long b, long long md) { return (!b ? 1 : (b & 1 ? a * power(a * a % md, b / 2, md) % md : power(a * a % md, b / 2, md) % md)); }
 #define db(x) cout << "\t\n" \
                    << #x << "\t" << x << "\t\n"
-#define dbarr(arr)           \
+#define dbarr(arr)            \
     cout << #arr << " ~ [ "; \
     for (auto n : arr)       \
         cout << n << " ";    \
     cout << "]" << endl;
-#define dbarrp(arr)                                \
+#define dbarrp(arr)                                 \
     cout << #arr << " ~ [ ";                       \
     for (auto n : arr)                             \
         cout << n.first << "-" << n.second << " "; \
     cout << "]" << endl;
 #define endl "\n"
-#define all(v) v.begin(), v.end()
+#define all(v) v.begin(),v.end()
 #define valueupto(x, y) fixed << setprecision(x) << y
 //#define PI 3.141592653589793238462
 typedef long long ll;
 #ifndef ONLINE_JUDGE
-#define debug(x)                          \
-    cerr << __LINE__ << " " << #x << " "; \
-    _print(x);                            \
+#define debug(x)       \
+    cerr<<__LINE__<<" " << #x << " "; \
+    _print(x);         \
     cerr << endl;
 #else
 #define debug(x)
@@ -52,52 +49,27 @@ void _print(vector<T> v)
     }
     cerr << "]";
 }
-// const int d4x[4] = {-1, 0, 1, 0}, d4y[4] = {0, 1, 0, -1};
-// const int d8x[8] = {-1, -1, 0, 1, 1, 1, 0, -1}, d8y[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+//const int d4x[4] = {-1, 0, 1, 0}, d4y[4] = {0, 1, 0, -1};
+//const int d8x[8] = {-1, -1, 0, 1, 1, 1, 0, -1}, d8y[8] = {0, 1, 1, 1, 0, -1, -1, -1};
 ////vector<int> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
 void solve()
 {
     int n, k, ans = 0, l, count = 0, sum = 0, mn = INT_MAX, mx = INT_MIN;
-    cin >> n >> k;
+    cin >> n  ;
 
-    unordered_map<int, int> mp;
-    vector<int> v(n, 0);
 
-    l = n - (k * 2);
-    for (int i = 0, x; i < n; ++i)
-    {
-        cin >> v[i];
-    }
 
-    {
-        sort(v.begin(), v.end());
 
-        for (int i = 0; i < l; ++i)
-        {
-            sum += v[i];
-        }
-    }
 
-    int tt = (n - l) >> 1;
-    int j = 1;
-    // debug(v);
 
-    for (int i = l; i < n; i++)
-    {
-        
-        mp[v[i]]++;
-        mx = max(mx, mp[v[i]]);
-    }
-    // db(mx);
-    if (mx>tt)
-    {
-        sum += mx - tt;
-        /* code */
-    }
-    
-    // db(tt);
-    cout << sum;
-    cout << "\n";
+
+
+
+
+
+
+
+
 }
 signed main()
 {
@@ -105,9 +77,6 @@ signed main()
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    int Test_Cases;
-    cin >> Test_Cases;
-    while (Test_Cases--)
-        solve();
+          solve();
     return 0;
 }
